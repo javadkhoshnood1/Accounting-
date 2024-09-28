0cfc6f60195b22e2007e29b5d1b2229701d08746
javad khoshnood <158989268+javad2566@users.noreply.github.com>
2024-09-28 10:29:50 +0330

    home app and header created

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include, re_path
+from django.conf.urls.static import static
+from core.settings import MEDIA_URL
+from core.settings import MEDIA_ROOT
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",include("home.urls"))
+]+ static(MEDIA_URL, document_root=MEDIA_ROOT)
---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+
+
+function coming_soon() {
+    Swal.fire("این قابلیت هنوز به سایت اضافه نشده ");
+    
+}
